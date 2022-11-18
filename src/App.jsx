47faecd87d08798c4d0ff39f0e63e0ffddfc9a96
santiago.herrera <santiagoah27@hotmail.com>
2022-11-18 09:03:26 -0500

    Se crea el grid para ver los resultados traidos de la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { AgGridReact } from 'ag-grid-react'
+import { useState, useEffect, useMemo } from 'react'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rowData, setRowData] = useState([])
+
+  const [columnDefs, setColumnDefs] = useState([
+    {field: 'employeeId'},
+    {field: 'firstName'},
+    {field: 'lastName'},
+    {field: 'age'},
+    {field: 'address'},
+    {field: 'email'},
+    {field: 'city'},
+    {field: 'timeWorkingInCompany'}
+  ])
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    filter: true
+  }), [])
+
+  useEffect(() => {
+    fetch('https://localhost:7192/api/users/Europa')
+    .then(result => result.json())
+    .then(rowData => setRowData(rowData))
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='ag-theme-alpine' style={{height: 500}}>
+    <AgGridReact
+       rowData={rowData}
+       columnDefs={columnDefs}
+       defaultColDef={defaultColDef}
+       animateRows={true}
+    />
     </div>
   )
 }
